9e33d2f10efdf4125cbb24d972cc11ed0c8b0d95
Angeles <angeles.vinicius@gmail.com>
2023-03-14 11:18:21 -0300

    first comit repository

---
+++ src/components/Navbar/Navbar.svelte
@@ -0,0 +1,13 @@
+<script>
+    import SmallNavbar from "./SmallNavbar.svelte";
+    import BigNavbar from "./BigNavbar.svelte";
+    let screenWidth;
+</script>
+
+<svelte:window bind:innerWidth={screenWidth} />
+{screenWidth}
+{#if screenWidth > 992}
+<BigNavbar />
+{:else}
+<SmallNavbar />
+{/if}
---
+++ src/pages/About.svelte
@@ -0,0 +1,1 @@
+<h1>welcome to About page</h1>
